a3263bcfc16ad93cf9bb2377fcfd97618cc1c509
belenviton <belenviton@hotmail.com>
2024-03-21 18:54:06 +0100

    resolved conexion, create first get endpoint, export create table

--- src/index.js
+++ src/index.js
@@ -18,10 +18,16 @@ server.use(express.json({ limit: '25mb' }));
 // CONFIGURACIÓN DE MYSQL
 
 async function getConnection() {
+  console.log({
+    host: process.env.MYSQL_HOST || 'localhost',
+    database: process.env.MYSQL_SCHEMA || 'recetas_db',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASS,
+  });
   const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_SCHEMA,
-    user: process.env.MYSQL_USER,
+    host: process.env.MYSQL_HOST || 'localhost',
+    database: process.env.MYSQL_SCHEMA || 'recetas_db',
+    user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASS,
   });
 
@@ -33,3 +39,36 @@ async function getConnection() {
 
   return connection;
 }
+
+// ARRANCAR EL SERVIDOR
+
+server.listen(port, () => {
+  console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
+});
+
+// ENDPOINTS
+
+getConnection();
+
+server.get('/api/recetas', async (req, res) => {
+  // Connectar con la base de datos
+
+  const conn = await getConnection();
+
+  // Lanzar un SELECT
+
+  const queryGetRecipes = `
+  SELECT * FROM recetas_db.recetas;
+  `;
+
+  const [results] = await conn.query(queryGetRecipes);
+
+  // Recuperar los datos
+
+  const numberOfElements = results.length;
+  // Cerramos la conexion
+
+  conn.close();
+
+  res.json({ info: { 'count': numberOfElements }, results: results });
+});
